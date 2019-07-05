0f1ef1f40268be4655c6919fa78b2734a0a8ee1c
Ruichen <huangruichen0226@gmail.com>
2019-07-05 14:53:33 -0400

    update goal

---
+++ models/Goal.js
@@ -0,0 +1,17 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var goal = Schema( {
+  userId: ObjectId,
+  cash: Number,
+  numDay: Number,
+  dailyHr: Number,
+  startAt: String,
+  endAt: String,
+  success: Number,
+  active: Boolean
+} );
+
+module.exports = mongoose.model( 'Goal', goalSchema );
