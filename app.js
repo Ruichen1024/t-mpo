11dcafe1e2e52c765a4a2dca5084dcdf40bd37a7
Ruichen <huangruichen0226@gmail.com>
2019-07-11 14:03:20 -0400

    init nap

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ flash = require('connect-flash')
 // END OF AUTHENTICATION MODULES
 
 const mongoose = require( 'mongoose' );
-mongoose.connect( 'mongodb://localhost/mydb' );
+mongoose.connect( 'mongodb://localhost/myDB' );
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -160,6 +160,10 @@ app.get('/daypage',isLoggedIn, (req,res)=>{
 
 app.get('/sleep',isLoggedIn, (req,res)=>{
   res.render('sleep', {title:"startSleep"})
+});
+
+app.get('/nap',isLoggedIn, (req,res)=>{
+  res.render('nap', {title:"nap"})
 });
 
 app.get('/tournament',isLoggedIn, (req,res)=>{
