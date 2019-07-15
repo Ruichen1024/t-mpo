f63649cdef1397fa6d4856a7fdc545a539040382
Ruichen <huangruichen0226@gmail.com>
2019-07-15 14:29:48 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '98976945157-iuikvrvl5nif5gcumv2v902c2cflnvcu.apps.googleusercontent.com',
         'clientSecret' : 'tYlTPSlMeCMrgrF5DPNSSl6e',
-        'callbackURL' : '	https://temposleep.herokuapp.com/login/authorized'
+        'callbackURL' : 'https://temposleep.herokuapp.com/login/authorized'
     }
 
 
