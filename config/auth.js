8d16e1d5065ee359d86ef7678ed7180a9fe42a07
tim <tjhickey@brandeis.edu>
2019-07-16 12:44:45 -0400

    cleaned up auth page

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,21 @@
+timauth = {
+       'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
+       'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
+	   'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
+    }
+
+richardauth = {
+    'clientID': '98976945157-iuikvrvl5nif5gcumv2v902c2cflnvcu.apps.googleusercontent.com',
+    'clientSecret' : 'tYlTPSlMeCMrgrF5DPNSSl6e',
+    //'callbackURL' : 'https://temposleep.herokuapp.com/login/authorized'
+    'callbackURL' : 'https://127.0.0.1:5500/login/authorized'
+}
+
+
 module.exports = {
 
 
-    'googleAuth' : {
-        'clientID': '98976945157-iuikvrvl5nif5gcumv2v902c2cflnvcu.apps.googleusercontent.com',
-        'clientSecret' : 'tYlTPSlMeCMrgrF5DPNSSl6e',
-        'callbackURL' : 'https://immense-headland-32722.herokuapp.com/login/authorized'
-
-        //'callbackURL' : 'https://temposleep.herokuapp.com/login/authorized'
-        //'callbackURL' : 'https://127.0.0.1:5500/login/authorized'
-    }
+    'googleAuth' : timauth
 
 
 
