5bbac8f0e68f138ae339e12e65b3cbb5f0ebbd67
tim <tjhickey@brandeis.edu>
2019-07-16 12:32:35 -0400

    updated auth

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,7 @@ module.exports = {
         'clientID': '98976945157-iuikvrvl5nif5gcumv2v902c2cflnvcu.apps.googleusercontent.com',
         'clientSecret' : 'tYlTPSlMeCMrgrF5DPNSSl6e',
         'callbackURL' : 'https://temposleep.herokuapp.com/login/authorized'
+        //'callbackURL' : 'https://127.0.0.1:5500/login/authorized'
     }
 
 
