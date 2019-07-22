c3f6554214f460fad540499d68988aca5e4ece1b
Ruichen <huangruichen0226@gmail.com>
2019-07-22 11:47:45 -0400

    fix missed items

--- config/apikey.js
+++ config/apikey.js
@@ -2,7 +2,6 @@ module.exports = {
 
     'apikey':{
         zipcode: 'cUVbjjOln90RuYOF9rPQ99BjOQvSrMv6STV604FWVXHhlmxrYXZw3YOOIGs5Pznn',
-        google: 'AIzaSyDA1MhAtoIWtPkv581uMiTBUsKlCYLQKbY'
     }
 
 };
